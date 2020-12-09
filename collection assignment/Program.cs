6543a79af56c4af61be273a632da9985fdae5b40
priyankas25 <65148864+priyankas25@users.noreply.github.com>
2020-12-09 21:44:51 +0530

    Add files via upload

---
+++ collection assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_assign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //simple interest...
+            Func<int, int, int, int> o = (p, n, r) => p * n * r / 100;
+
+            Func<int, int, bool> o1 = ( l, m) => l > m;
+
+            Predicate<int> o2 = a => a % 2 == 0;
+
+            Console.WriteLine(o(3000,4,5)+" "+o1(6,5)+" "+o2(7));
+            Console.ReadLine();
+        }
+    }
+}
